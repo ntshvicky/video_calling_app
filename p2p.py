08949fbda721877aec5bdd20fa8e9c0a2aac7d6f
Nitish Srivastava <nitish.srivastava@mavenai.in>
2023-10-25 13:25:10 +0400

    added mute in local camera

--- p2p.py
+++ p2p.py
@@ -4,8 +4,8 @@ from flask_socketio import SocketIO, send, emit
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-@app.route('/')
-def index():
+@app.route('/<meet_id>')
+def index(meet_id):
     return render_template('p2p.html')
 
 @socketio.on('signal')
